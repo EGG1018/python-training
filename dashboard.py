f73123cc40629077632147e636180ed69c78a14e
EGG1018 <as775877@gmail.com>
2023-07-13 14:56:32 +0800

    EGG

---
+++ dashboard.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 載入碳排放數據
+emissions_data = pd.read_csv('碳排放數據.csv')
+
+# 在側邊欄添加選項
+st.sidebar.title('碳排放 Dashboard')
+visualization = st.sidebar.selectbox('個人分析', ('數據表格', '折線圖'))
+
+if visualization == '數據表格':
+    # 顯示碳排放數據表格
+    st.header('碳排放數據表格')
+    st.dataframe(emissions_data)
+else:
+    # 顯示碳排放折線圖
+    st.header('碳排放趨勢折線圖')
+    plt.plot(emissions_data['年份'], emissions_data['碳排放量'])
+    plt.xlabel('年份')
+    plt.ylabel('碳排放量')
+    plt.title('碳排放趨勢')
+    st.pyplot(plt)
+
+
+
+
